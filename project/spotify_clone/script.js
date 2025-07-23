26405ccc899ccdbd40734bdd454fe715fc6223e4
Shivam Sharma <bosssks593@gmail.com>
2025-07-23 12:19:03 +0530

    Add files via upload

---
+++ project/spotify_clone/script.js
@@ -0,0 +1,9 @@
+let button = document.querySelector('.button1');
+button.onclick = (e) => {
+    window.location.href = './login.html';
+}
+
+let button1 = document.querySelector('.button2');
+button1.onclick = (e) => {
+    window.location.href = './signup.html';
+}
